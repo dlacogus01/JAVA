a7084b96b2e7258b40fc7ff02a21e73f40f19d88
dlacogus01 <65270396+dlacogus01@users.noreply.github.com>
2021-06-04 09:56:08 +0900

    Add files via upload

---
+++ ArrayReview/src/ForEachEX.java
@@ -0,0 +1,24 @@
+
+
+public class ForEachEX {
+
+	public static void main(String[] args) {
+		int intArray[] = {6, 2, 1, 140, 50};
+		String strArray[] = {"Apple", "Cherry", "Manggo", "Orange", "Banana"};
+		int count = 0, sum =0;
+		for(int i : intArray) {
+			System.out.println(i);
+			sum += i;
+		count++;
+		if(count < intArray.length)
+			System.out.print(" + ");
+		else
+			System.out.println(" = ");
+		}
+		System.out.println(sum);
+		for(String str : strArray)
+			System.out.println(str);
+	}
+	
+
+}
